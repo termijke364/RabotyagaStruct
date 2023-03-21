3a051a9fe6ccf28c184c28ddfcf19c79bdece038
Nikita Pepelyaev <termi364@mail.ru>
2023-03-21 17:36:20 +0400

    Add project files.

---
+++ RabotyagaStruct/RabotyagaStruct.cpp
@@ -0,0 +1,62 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <string.h>
+#include <string>
+
+using namespace std;
+
+struct Employe
+{
+	string name;
+	string carier;
+	int age;
+};
+
+enum Position
+{
+	Director = 1,
+	Accountant,
+	Handyman
+};
+
+int main()
+{
+	setlocale(0, "");
+	SetConsoleCP(1251);
+
+	string name;
+	cout << "Введите имя работника: " << endl;
+	getline(cin, name);
+
+	cout << "Выберите должность работника: " << endl;
+	cout << "1 - Director " << endl;
+	cout << "2 - Accountant " << endl;
+	cout << "3 - Handyman " << endl;
+	int point;
+	string carier;
+	cin >> point;
+	switch (point)
+	{
+	case Director:
+		carier = "Director";
+		break;
+	case Accountant:
+		carier = "Accountant";
+		break;
+	case Handyman:
+		carier = "Handyman";
+		break;
+	default:
+		break;
+	}
+
+	cout << "Введите возраст работника: " << endl;
+	int age;
+	cin >> age;
+
+	Employe rabotniki{ name, carier, age };
+
+	cout << "Имя работника: " << rabotniki.name << "\t" << "| Должность работника: " << rabotniki.carier << "\t" << "| Возраст работника: " << rabotniki.age << endl;
+
+	return 0;
+}
